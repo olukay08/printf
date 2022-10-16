9c6981824cd4ec36f5435b65de7fb60abafacf61
oluwole2044 <olukay08@gmail.com>
2022-10-16 13:35:51 -0700

    creating printf

---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef PRINT_F
+#define PRINT_F
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * struct convert - defines a structure for symbols and functions
+ * @sym: The operator
+ * @f: The function associated
+ */
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert con_t;
+int _putchar(char);
+int _printf(const char *format, ...);
+int convert_func(const char *format, con_t f_list[], va_list arg_list);
+int print_char(va_list list);
+int print_string(va_list list);
+int print_percent(__attribute__((unused))va_list list);
+#endif
